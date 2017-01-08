614c9edfc0e10244aa40b4bd3bdb5aeb496d3cb9
“Sinyru” <znchen720@gmail.comgit config --global pull.rebase truegit config --global branch.autosetuprebase alwaysgit config --global push.default simplegit config --global branch.autosetupmerge truegit config --global core.editor atom>
2017-01-07 22:46:23 -0500

    Updated 3 files and added 2 images

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,46 @@
-console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var gameBoard = document.getElementById("game-board");
 
-if (cardOne === cardTwo)
-	alert ("You found a match!");
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+
+var isMatch = function(){
+	if (cardsInPlay[0]==cardsInPlay[1])
+		alert("You Found a Match!");
+	else
+		alert ("Try Again");
+	reset();
+}
+
+var isTwoCards = function() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') == "king")
+		this.innerHTML = '<img src="king.png" alt="King" />';
+	else
+		this.innerHTML = '<img src="queen.png" alt="Queen" />';
+	if (cardsInPlay.length === 2) {
+		isMatch();
+		cardsInPlay = [];
+	}
+}
+
+
+var createBoard = function(){
+	for (var i = 0; i< cards.length; i++) {
+		var cardElement = document.createElement("div");
+		cardElement.className = "card";
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
+        gameBoard.appendChild(cardElement);
+	}
+}
+
+var reset = function(){
+	for (var i = 0; i < 4; i++) {
+		document.getElementsByClassName("card")[i].innerHTML = "";
+	}
+}
+
+createBoard();
